c3258e80c82d8c89bf75a7e6fb13f07be4923277
radionov <boss@flagstudio.ru>
2018-04-07 16:36:10 +0500

    v0

--- ps1.sh
+++ ps1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# set -e
+set -e
 
 # Colors
 green='\033[1;32m'       # 1 Green
@@ -72,3 +72,78 @@ else
 fi
 
 PS1=" ${user_name}${host_name} ${path} "
+
+BEGIN='HELLO_BASH_BEGIN'
+END='HELLO_BASH_END'
+SOURCE=$PS1
+
+
+#
+#
+# Showing command execution status to user
+#
+#
+
+check_command_exec_status () {
+  if [ $1 -eq 0 ]
+    then
+      echo -e "${yellow}SUCCESS!${NC}"
+      echo
+      sleep 1
+  else
+    echo -e "${red}ERROR${NC}"
+    echo
+
+  fi
+}
+
+#
+#
+# INSTALLER
+#
+#
+
+
+  TOTAL_LINES=`cat ~/.bashrc | wc -l`
+  BEGIN_LINE=`grep -n -e ${BEGIN} ~/.bashrc | cut -d : -f 1`
+  END_LINE=`grep -n -e ${END} ~/.bashrc | cut -d : -f 1`
+  TAIL_LINES=$(($TOTAL_LINES-$END_LINE + 1))
+
+  # Can't find both GGA markers
+  if [[ ! $BEGIN_LINE ]] && [[ ! $END_LINE ]] || [[ $BEGIN_LINE ]] && [[ $END_LINE ]]
+  then
+    echo ''
+    echo -e "All right! Let's do it!";
+    echo ''
+    echo "Making backup of your '~/.bashrc' in ~/.bashrc.backup ...";
+    echo ''
+    cp ~/.bashrc ~/.bashrc.backup
+    check_command_exec_status $?
+    echo ''
+    echo "Installing in 3 steps ...";
+    echo ${BEGIN}>> ~/.bashrc
+    check_command_exec_status $?
+    curl -L -s ${SOURCE} >> ~/.bashrc
+    check_command_exec_status $?
+    echo ${END}>> ~/.bashrc
+    check_command_exec_status $?
+    # . ~/.bashrc
+
+    echo 'Self-terminating'
+    rm -f ${0##*/}
+    check_command_exec_status $?
+    echo -e "Now restart your terminal or run this (yes, dot is a command):"
+    echo ""
+    echo -e "${cyan}. ~/.bashrc${NC}";
+    echo ""
+    exit 0;
+  fi
+
+  # One of markers is broken
+  if [[ ! $BEGIN_LINE ]] || [[ ! $END_LINE ]]
+  then
+    echo -e "It looks like one of two GGA markers is broken. Hmm.. I guess you'll need to fix it yourself.\n You must check that ${red}${BEGIN}${NC} is placed in the beginning and ${red}${END}${NC}  in the end of Go Git Aliases block.\n Self-terminating.";
+    rm -f ${0##*/}
+    check_command_exec_status $?
+    exit 0;
+  fi
