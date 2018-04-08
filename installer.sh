7f29a79f4af67693210dfa8844b3c69777897777
radionov <boss@flagstudio.ru>
2018-04-08 14:26:17 +0500

    0

--- installer.sh
+++ installer.sh
@@ -41,6 +41,6 @@ echo ''
 rm -f ${0##*/}
 check_command_exec_status $?
 echo ''
-echo 'Done! Do not forget to source your ~/.bashrc by typing:'
+echo -e 'Done! Do not forget to source your ~/.bashrc by typing:'
 echo ''
 echo 'source \033[1;35m ~/.bashrc \033[0m'
